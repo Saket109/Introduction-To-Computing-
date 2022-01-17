bfdebc22267b8578f0bbb97dbd25204924fb364b
Saket109 <96969714+Saket109@users.noreply.github.com>
2022-01-18 01:28:33 +0530

    Add files via upload

---
+++ assignment2solutions.py
@@ -0,0 +1,41 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: C:/Users/jk/Music/assignment2.py ==================
+QUESTION 1.)
+PART A
+35
+PART B
+egaugnal evitisnes esac a si nohtyP
+PART C
+SLICED STRING IS:  a case sensitive
+PART D
+Python is object oriented language
+PART E
+10
+PART F
+Pythonisacasesensitivelanguage
+QUESTION 2.)
+Hey,Saket Ahlawat Here!
+My SID is 21105105
+I am from ECE department and my CGPA is 10
+QUESTION 3.)
+a&b is:  8
+a|b is:  58
+a^b is:  50
+a<<2: 224 	b<<2:  40
+a>>2: 14 	b>>4:  0
+QUESTION 4.)
+ENTER THE FIRST NUMBER: 43
+ENTER THE SECOND NUMBER: 67
+ENTER THE THIRD NUMBER: 90
+THE GREATEST NUMBER IS 90
+QUESTION 5.)
+ENTER ANY STRINGsaket ahlawat
+NO
+QUESTION 6.)
+ENTER LENGTH OF FIRST SIDE OF TRIANGLE: 32
+ENTER LENGTH OF SECOND SIDE OF TRIANGLE: 21
+ENTER LENGTH OF THIRD SIDE OF TRIANGLE: 30
+YES
+>>> 
