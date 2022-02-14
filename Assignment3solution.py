9ef4eb78842b11c32255396a3791958ca89ef2ef
Saket109 <96969714+Saket109@users.noreply.github.com>
2022-02-14 18:33:42 +0530

    Add files via upload

---
+++ Assignment3solution.py
@@ -0,0 +1,97 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================================================= RESTART: C:/Users/jk/Pictures/pythonidle/Assignment3.py =======================================================
+THE SOLUTION OF QUESTION 1 :
+
+Enter the string: saket is read as saket and written as saket
+The occurence(s) of:
+		"saket"    is/are   3 
+		"is"    is/are   1 
+		"read"    is/are   1 
+		"as"    is/are   2 
+		"and"    is/are   1 
+		"written"    is/are   1 
+
+THE SOLUTION OF QUESTION 2 :
+
+Day - 2
+Month - 3
+Year - 1999
+Next Date is: 3/03/1999
+
+THE SOLUTION OF QUESTION 3 :
+
+Enter the list: 2,4,5
+Output: [(2, 4), (4, 16), (5, 25)]
+
+THE SOLUTION OF QUESTION 4 :
+
+ENTER YOUR GRADE : 9
+YOUR GRADE IS A AND EXCELLENT PERFORMANCE
+
+THE SOLUTION OF QUESTION 5 IS :
+
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+
+THE SOLUTION OF QUESTION 6 IS :
+
+ENTER THE NAME : saket
+ENTER THE SID OF saket: 21105105
+
+You have entered 1 value(s) 
+Do you want to enter more data? YES
+ENTER THE NAME : rishabh
+ENTER THE SID OF rishabh: 21105114
+
+You have entered 2 value(s) 
+Do you want to enter more data? y
+ENTER THE NAME : divyam
+ENTER THE SID OF divyam: 21105099
+
+You have entered 3 value(s) 
+Do you want to enter more data? no
+PART A
+	THE SID OF 'saket'   is   '21105105'
+	THE SID OF 'rishabh'   is   '21105114'
+	THE SID OF 'divyam'   is   '21105099'
+PART B
+THE SID OF 'divyam'   is   '21105099'
+THE SID OF 'rishabh'   is   '21105114'
+THE SID OF 'saket'   is   '21105105'
+PART C
+THE SID OF 'divyam'   is   '21105099'
+THE SID OF 'saket'   is   '21105105'
+THE SID OF 'rishabh'   is   '21105114'
+PART D
+ENTER SID WHICH YOU WANT TO SEARCH : 21105105
+THE NAME OF THE STUDENT WHOSE SID IS '21105105' IS 'saket' 
+
+THE SOLUTION OF QUESTION 7 IS :
+
+HOW MANY TERMS OF FIBONACCI SERIES DO YOU WANT : 8
+1 1 2 3 5 8 13 21 
+Average is : 6.75
+
+THE SOLUTION OF QUESTION 8 :
+
+PART A 
+{1, 3, 5, 6, 8}
+
+PART B 
+{17, 3, 6, 8, 9, 13}
+
+PART C 
+{1, 2, 4, 5}
+
+PART D 
+{6, 7, 8, 9, 10}
+
+PART E 
+{10, 7}
+>>> 
