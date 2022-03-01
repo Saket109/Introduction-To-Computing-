7bdf6ee37c9667cc1981e29cd3f1b94db2413ed6
Saket109 <96969714+Saket109@users.noreply.github.com>
2022-03-02 00:29:24 +0530

    Add files via upload

---
+++ Assignment 4 solutions.py
@@ -0,0 +1,81 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================================================= RESTART: C:/Users/jk/Pictures/pythonidle/Assignment 4.py ======================================================
+QUESTION 1 :
+
+Move 1th disk from A to C
+Move 2th disk from A to B
+Move 1th disk from C to B
+Move 3th disk from A to C
+Move 1th disk from B to A
+Move 2th disk from B to C
+Move 1th disk from A to C
+
+QUESTION 2 :
+
+ENTER A NUMBER :6
+
+Using Loop
+
+     1     
+    1 1    
+   1 2 1   
+  1 3 3 1  
+ 1 4 6 4 1 
+1 5 10 10 5 1
+
+Using recursions:
+
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+QUESTION 3
+
+ENTER TWO INTEGERS SEPERATED BY SPACE : 32 3
+(10, 2)
+Part A
+True
+
+Part B
+True
+
+Part C
+New Tuple : (10, 2, 4, 5, 6)
+Greater Than 4 : [10, 5, 6]
+
+Part D
+{2, 4, 5, 6, 10}
+
+Part E
+FROZEN SET : frozenset({2, 4, 5, 6, 10})
+
+Part F
+10
+
+QUESTION 4 :
+Object Created!!
+The name of the student is Saket and it's rollno is 21105105
+Object Destroyed!!
+
+QUESTION 5 :
+
+Part A
+The updated salary of Mehak is 70000
+
+Part B
+Viren record deleted!!
+
+QUESTION 6
+
+Enter word uttered by George: hence
+Enter word made by Barbie: hnece
+
+Do you want to consider same no. of letters also? y
+
+Barbie has made a valid word!  HENCE THEIR FRIENDSHIP IS TRUE.
+>>> 
